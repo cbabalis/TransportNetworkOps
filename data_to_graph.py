79f5fb9d0f8677f7358d395196a04c8cfde0b1be
Babis Babalis <babisbabalis@gmail.com>
2019-03-04 21:23:58 +0200

    gitignore updated. data_to_graph.py created.

---
+++ data_to_graph.py
@@ -0,0 +1,13 @@
+# script which imports a .dbf file, converts it to graph and writes
+# the results to json/csv.
+
+
+def main():
+    # read the file and convert it to dictionary
+    # create the graph
+    # write the results to json file
+    pass
+
+
+if __name__ == '__main__':
+    main()
